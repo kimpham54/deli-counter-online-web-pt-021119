ecc343efe5d8b1c881db3e4fd1aab185f34bf94d
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-28 16:17:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
+clone_another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(array)
@@ -23,27 +25,63 @@ def take_a_number(array,name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
-def now_serving(array)
-  arrayb = array
-  if arrayb.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    arrayb.each do |linemate|
-      hey = arrayb.delete_at(0)
-      # puts hey
-      # puts linemate
-      # puts "Currently serving #{hey}."
-      print(*arrayb)
-      print('arrayb now array')
-      print(*array)
+# def now_serving(array)
+#   arrayb = array
+#   if arrayb.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     arrayb.each do |linemate|
+#       hey = arrayb.delete_at(0)
+#       # puts hey
+#       # puts linemate
+#       # puts "Currently serving #{hey}."
+#       # print(*arrayb)
+#       # print('arrayb now array')
+#       print(*array)
       
+#     end
+#   end
+# end
+# another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+
+
+# def now_serving(array)
+#   arrayb = array
+#   if arrayb.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     until clone_another_deli.empty? == true
+#       if another_deli.include?(clone_another_deli[0]) == true
+
+      
+#     end
+#   end
+# end
+
+def now_serving(array, arrayclone)
+  if array.length == 0
+    puts "There is nobody waiting to be seved!"
+  else
+    arrayclone.each do |linemate|
+      if array.include?(linemate) == true
+        array.shift
+        print("next in line: " + linemate + "\n")
+        puts array.inspect
+        puts arrayclone.inspect
+      end
     end
   end
 end
-# another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+
+
+
+
+
 
 
 # line(katz_deli)
 # take_a_number(katz_deli, 'girl')
-now_serving(other_deli)
-# print(*other_deli)
+now_serving(another_deli, clone_another_deli)
+# print(*another_deli)
