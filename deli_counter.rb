3891045eabb0ad8fa3124d8e7977eb000e4fe2cf
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-28 16:38:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def take_a_number(array,name)
 end
 
 def now_serving(array)
-  if array.empty?
+  if array.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
